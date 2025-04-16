8cdee14df6a9286cddacf3d88ef459cf4a3ebf9f
Dan Johns <danjhd@amazon.com>
2025-04-16 14:00:50 +0100

    implement batch processing of SQS

--- backend/components/ingest/functions/sqs-segment-ingestion/app.py
+++ backend/components/ingest/functions/sqs-segment-ingestion/app.py
@@ -7,14 +7,20 @@ import boto3
 import requests
 from aws_lambda_powertools import Logger, Metrics, Tracer, single_metric
 from aws_lambda_powertools.metrics import MetricUnit
-from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent, SQSRecord
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.batch import (
+    BatchProcessor,
+    EventType,
+    process_partial_response,
+)
 from botocore.exceptions import ClientError
 from openid_auth import Credentials
 
 tracer = Tracer()
 logger = Logger()
-metrics = Metrics(namespace="Powertools")
+metrics = Metrics()
+batch_processor = BatchProcessor(event_type=EventType.SQS)
 
 s3 = boto3.client("s3")
 endpoint = os.environ["TAMS_ENDPOINT"]
@@ -129,46 +135,50 @@ def delete_s3_file(source: str) -> None:
                 logger.error(ex)
 
 
+@tracer.capture_method(capture_response=False)
+def record_handler(record: SQSRecord) -> None:
+    """Processes a single SQS record"""
+    message = json.loads(record.body)
+    sent_timestamp = datetime.fromtimestamp(
+        int(record.attributes.sent_timestamp) / 1000
+    )
+    first_receive_timestamp = datetime.fromtimestamp(
+        int(record.attributes.approximate_first_receive_timestamp) / 1000
+    )
+    receive_delta_seconds = (first_receive_timestamp - sent_timestamp).total_seconds()
+    logger.info(f"Approximate receive delta: {receive_delta_seconds}")
+    with single_metric(
+        name="SQSIngestReceiveDelta",
+        unit=MetricUnit.Seconds,
+        value=receive_delta_seconds,
+    ) as metric:
+        metric.add_dimension(
+            name="base_uri", value="/".join(message["uri"].split("/")[:-1])
+        )
+    flow_id = message["flowId"]
+    file_data = get_file(message["uri"], message.get("byterange", None))
+    if not file_data:
+        raise ValueError(f'Unable to read source file {message["uri"]}')
+    media_object = upload_file(flow_id, file_data)
+    if media_object is None:
+        raise ValueError(f"Unable to upload file to flow {flow_id}")
+    if media_object["put_url"]["content-type"].split("/")[0] == "image":
+        message["timerange"] = f'{message["timerange"].split("_")[0]}]'
+    post_result = post_segment(flow_id, media_object["object_id"], message["timerange"])
+    if not post_result:
+        raise ValueError(f"Unable to post segment to flow {flow_id}")
+    if message.get("deleteSource", False):
+        delete_s3_file(message["uri"])
+
+
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler(capture_response=False)
 @metrics.log_metrics(capture_cold_start_metric=True)
 # pylint: disable=unused-argument
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
-    event: SQSEvent = SQSEvent(event)
-    for record in event.records:
-        message = json.loads(record.body)
-        sent_timestamp = datetime.fromtimestamp(
-            int(record.attributes.sent_timestamp) / 1000
-        )
-        first_receive_timestamp = datetime.fromtimestamp(
-            int(record.attributes.approximate_first_receive_timestamp) / 1000
-        )
-        receive_delta_seconds = (
-            first_receive_timestamp - sent_timestamp
-        ).total_seconds()
-        logger.info(f"Approximate receive delta: {receive_delta_seconds}")
-        with single_metric(
-            namespace="TAMS-Tools",
-            name="SQSIngestReceiveDelta",
-            unit=MetricUnit.Seconds,
-            value=receive_delta_seconds,
-        ) as metric:
-            metric.add_dimension(
-                name="base_uri", value="/".join(message["uri"].split("/")[:-1])
-            )
-        flow_id = message["flowId"]
-        file_data = get_file(message["uri"], message.get("byterange", None))
-        if not file_data:
-            raise ValueError(f'Unable to read source file {message["uri"]}')
-        media_object = upload_file(flow_id, file_data)
-        if media_object is None:
-            raise ValueError(f"Unable to upload file to flow {flow_id}")
-        if media_object["put_url"]["content-type"].split("/")[0] == "image":
-            message["timerange"] = f'{message["timerange"].split("_")[0]}]'
-        post_result = post_segment(
-            flow_id, media_object["object_id"], message["timerange"]
-        )
-        if not post_result:
-            raise ValueError(f"Unable to post segment to flow {flow_id}")
-        if message.get("deleteSource", False):
-            delete_s3_file(message["uri"])
+def lambda_handler(event: SQSEvent, context: LambdaContext) -> dict:
+    return process_partial_response(
+        event=event,
+        record_handler=record_handler,
+        processor=batch_processor,
+        context=context,
+    )
