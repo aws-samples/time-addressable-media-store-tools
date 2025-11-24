7212f07dae27d2288778ad5c8022c8e9821659aa
Dan Johns <danjhd@amazon.com>
2025-11-24 12:36:33 +0000

    AWS SDK calls use temporary credentials

--- frontend/src/hooks/useParameters.jsx
+++ frontend/src/hooks/useParameters.jsx
@@ -1,17 +1,22 @@
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 import useSWR from "swr";
 import { AWS_REGION } from "@/constants";
-import { fetchAuthSession } from "aws-amplify/auth";
+import useAwsCredentials from "@/hooks/useAwsCredentials";
 
 export const useParameter = (parameterName) => {
+  const credentials = useAwsCredentials();
   const { data, error, isLoading } = useSWR(
     ["/ssm-parameters", parameterName],
-    ([, parameterName]) =>
-      fetchAuthSession().then((session) =>
-        new SSMClient({ region: AWS_REGION, credentials: session.credentials })
-          .send(new GetParameterCommand({ Name: parameterName }))
-          .then((response) => JSON.parse(response.Parameter.Value))
-      )
+    async ([, parameterName]) => {
+      const client = new SSMClient({
+        region: AWS_REGION,
+        credentials,
+      });
+      const response = await client.send(
+        new GetParameterCommand({ Name: parameterName })
+      );
+      return JSON.parse(response.Parameter.Value);
+    }
   );
 
   return {
