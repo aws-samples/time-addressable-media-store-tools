1012e5564f9f04fef96066d32a8c7ca30bd12436
Dan Johns <danjhd@amazon.com>
2026-03-30 17:54:35 +0100

    remove remaining js stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import path from 'path';
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
